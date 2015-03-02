4003569d78e826888b2c526ee26a3c1beaea24a8
Kim seung-il <hanulo78@gmail.com>
2015-03-03 07:40:00 +0900

    Update and rename backbone-study/js/home/views to backbone-study/js/home/views/MainView.js

---
+++ backbone-study/js/home/views/MainView.js
@@ -0,0 +1,26 @@
+define([
+    'backbone',
+    './HeaderView',
+    './FooterView'
+], function (Backbone, HeaderView, FooterView) {
+
+	var MainView = Backbone.View.extend({
+		initialize: function () {
+			this.subviews = [];
+		},
+
+		render: function () {
+			var headerView = new HeaderView();
+			this.subviews.push(headerView);
+			this.$el.append(headerView.render().el);
+
+			var footerView = new FooterView();
+			this.subviews.push(footerView);
+			this.$el.append(footerView.render().el);
+
+			return this;
+		}
+	});
+
+	return MainView;
+});
