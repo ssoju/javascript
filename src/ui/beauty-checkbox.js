cbb2474780686679af1cf42f7800fd5e3c077e28
ssoju <hanulo78@gmail.com>
2015-05-11 22:11:05 +0900

    .

--- src/ui/beauty-checkbox.js
+++ src/ui/beauty-checkbox.js
@@ -7,49 +7,23 @@
 (function (ctx, $, core, undefined) {
     "use strict";
 
-    core.beautyCheckbox = function(selector) {
-        selector || (selector = 'input[type=checkbox]:not(._rc_), input[type=radio]:not(._rc_)');
 
-        var $items = $(selector).not('._rc_').addClass('_rc_');
-        $items.wrap('<span></span>').parent()
-            .addClass('ui_radiocheck')
-            .css({'display': 'inline-block', 'overflow': 'hidden', 'position': 'relative'});
-
-        $items.css({
-            position: 'absolute',
-            left: '-10%',
-            top: '-10%',
-            width: '120%',
-            height: '120%',
-            opacity: 0
-        }).each(function(){
-            $(this).parent().toggleClass('checked', $(this).prop('checked'));
-        });
-    };
-
-    $(function(){
-
-        $(document).on('click', '', function(e) {
-            var $el = $(this);
-            $el.parent().toggleClass('checked', $el.prop('checked'));
-        }).on('click', 'input._rc_[type=radio], input[type=checkbox]._rc_', function(e){
-            var $el = $(this);
-            if($el.is('[type=radio]')) {
-                $(this.form).find('._rc_[name="' + this.name + '"]').not(this).parent().removeClass('checked');
-            }
-            $el.parent().toggleClass('checked', $el.prop('checked'));
-        }).on('focusin focusout', 'input._rc_[type=checkbox], input._rc_[type=radio]', function(e) {
-            switch(e.type) {
-                case 'focusin':
-                    $(this).parent().addClass('focus');
-                    break;
-                case 'focusout':
-                    $(this).parent().removeClass('focus');
-                    break;
-            }
-        });
-
-    });
+	$(document).on('click', 'input[type=radio], input[type=checkbox]', function(e) {
+		var $el = $(this);
+		$el.parent().toggleClass('checked', $el.prop('checked'));
+		if($el.is('[type=radio]')) {
+			$(this.form).find('[name="' + this.name + '"]').not(this).parent().removeClass('checked');
+		}
+	}).on('focusin focusout', 'input[type=checkbox], input[type=radio]', function(e) {
+		switch(e.type) {
+			case 'focusin':
+				$(this).parent().addClass('focus');
+				break;
+			case 'focusout':
+				$(this).parent().removeClass('focus');
+				break;
+		}
+	});
 
 
 })(window, jQuery, window[LIB_NAME]);
