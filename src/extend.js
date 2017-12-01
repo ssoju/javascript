f38d24fe6ffd27c13e91583857f55b280334c690
Gilbert <hanulo78@gmail.com>
2017-12-02 01:16:26 +0900

    Update extend.js

--- src/extend.js
+++ src/extend.js
@@ -42,3 +42,43 @@
                 return target;
             }
         })(),
+                
+       ////////////////
+                
+// 더 짧은 거: 이제 더이상 못 줄이겠당.
+      extend = (function () { 
+            function type(obj, val) {
+                return Object.prototype.toString.call(obj).toLowerCase() === '[object ' + val + ']';
+            }
+
+            function isPlainObject(value) {
+                return value !== null
+                    && value !== undefined
+                    && type(value, 'object')
+                    && value.ownerDocument === undefined;
+            } 
+            return function (deep, target) {
+                var objs;
+                if (typeof deep === 'boolean') {
+                    objs = [].slice.call(arguments, 2);
+                } else {
+                    objs = [].slice.call(arguments, 1);
+                    target = deep;
+                    deep = false;
+                }
+                        
+                each(objs, function (obj) {
+                    if (!obj || (!isPlainObject(obj) && !type(obj, 'array'))) { return; }
+                    each(obj, function (val, key) {     
+                        var isArr = type(val, 'array');   
+                        if (deep === true && (isArr || isPlainObject(val))) {
+                            target[key] = extend(deep, target[key] || (target[key] = isArr ? [] : {}), val);
+                            return;
+                        }
+                        target[key] = val;
+                    });
+                });
+                return target;
+            }
+        })(),
+
